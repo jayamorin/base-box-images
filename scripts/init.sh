25efcafdfc8ed943e89928a2f7f1d50cf9d30dea
Jay Amorin <jayamorin@users.noreply.github.com>
2019-07-13 16:45:10 +0800

    install VBoxGuestAdditions

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,3 +2,8 @@
 
 DEBIAN_FRONTEND=noninteractive apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+
+mount -o loop,ro /home/vagrant/VBoxGuestAdditions_`cat /home/vagrant/.vbox_version`.iso /media/cdrom
+sh /media/cdrom/VBoxLinuxAdditions.run
+umount -l /media/cdrom
+rm -f /home/vagrant/VBoxGuestAdditions_`cat /home/vagrant/.vbox_version`.iso
